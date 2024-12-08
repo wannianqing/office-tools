d5b4b0f9dac40e609afc8748b1b08b20a368db63
wannianqing <nianqing.wan@kingsunsoft.com>
2024-12-08 15:00:30 +0800

    静态页

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: '/examples/',
+  publicPath: './',
   transpileDependencies: true,
   assetsDir:'',
   chainWebpack: (config) => {
