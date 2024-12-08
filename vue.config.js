71112c01da8b95a56574b28f2bb87b26d123179e
wannianqing <nianqing.wan@kingsunsoft.com>
2024-12-08 15:19:53 +0800

    ef

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: '/office-tools/',
+  publicPath: '/office-tools/dist/',
   transpileDependencies: true,
   assetsDir:'',
   chainWebpack: (config) => {
